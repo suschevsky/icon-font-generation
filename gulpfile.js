3356a2bafe9608f193b2013efe20f1cad07f0567
suschevsky <suschevsky.sv@gmail.com>
2017-03-30 00:46:15 +0500

    Add icon font generation

--- gulpfile.js
+++ gulpfile.js
@@ -3,38 +3,23 @@ var webserver = require('gulp-webserver');
 var opn = require('opn');
 var tap = require('gulp-tap');
 var fs = require('fs');
+var iconfont = require('gulp-iconfont');
+var iconfontCss = require('gulp-iconfont-css');
+var svgmin = require('gulp-svgmin');
+var path = require('path');
 
 var server = {
     host: 'localhost',
     port: 4000
 };
 
-gulp.task('default', ['build', 'webserver', 'opn']);
+var fontName = 'my-icon-font';
 
-gulp.task('build', ['html', 'opn']);
-
-gulp.task('html', function() {
-    return gulp.src(['./src/index.html'])
-        .pipe(gulp.dest('./dist'));
-
-});
-
-gulp.task('webserver', ['html'], function() {
-    gulp.src( 'dist' )
-        .pipe(webserver({
-            host: server.host,
-            port: server.port,
-            livereload: true,
-            directoryListing: false
-        }));
-});
-
-gulp.task('opn', function() {
-    opn( 'http://' + server.host + ':' + server.port );
-});
+gulp.task('default', ['write-filenames-to-js-array', 'iconfont', 'html', 'webserver', 'opn']);
 
 gulp.task('write-filenames-to-js-array', function() {
-    var jsFilename = "dist/iconList.js";
+
+    var jsFilename = 'dist/iconList.js';
     fs.writeFile(jsFilename);
 
     var iconList = fs.createWriteStream(jsFilename, {'flags': 'w'});
@@ -46,4 +31,42 @@ gulp.task('write-filenames-to-js-array', function() {
         .pipe(tap(function(file, t) {
             iconList.write('iconList.push("' + file.path.match(iconFileRegex)[0] + '");\n');
         }));
+});
+
+gulp.task('iconfont', function() {
+    gulp.src(['assets/*.svg'])
+        .pipe(svgmin())
+        .pipe(iconfontCss({
+            fontName: fontName,
+            path: 'src/icons-template.ejs',
+            fontPath: '/fonts/' + fontName + '/',
+            targetPath: path.resolve(__dirname, 'dist') + '/css/' + fontName + '.css'
+
+        }))
+        .pipe(iconfont({
+            normalize: true,
+            fontName: fontName,
+            appendCodepoints: true
+        }))
+        .pipe(gulp.dest('dist/fonts/' + fontName));
+});
+
+gulp.task('html', function() {
+    return gulp.src(['./src/index.html'])
+        .pipe(gulp.dest('./dist'));
+
+});
+
+gulp.task('webserver', function() {
+    gulp.src( 'dist' )
+        .pipe(webserver({
+            host: server.host,
+            port: server.port,
+            livereload: true,
+            directoryListing: false
+        }));
+});
+
+gulp.task('opn', function() {
+    opn( 'http://' + server.host + ':' + server.port );
 });
